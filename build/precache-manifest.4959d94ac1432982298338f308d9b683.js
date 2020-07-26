1530d2deb03d772edbd548e25e7861de961ed540
tinshade <tinshade98@gmail.com>
2020-07-26 13:38:29 +0530

    Adding production build

---
+++ build/precache-manifest.4959d94ac1432982298338f308d9b683.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "17df5eedb08a379d5d5ce75c79e921a3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bf07f8fbae924dfa34cb",
+    "url": "/static/css/2.829c9cb5.chunk.css"
+  },
+  {
+    "revision": "bf07f8fbae924dfa34cb",
+    "url": "/static/js/2.523baf1c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.523baf1c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b49eb897ce7c969a9a8b",
+    "url": "/static/js/main.3ffca1b6.chunk.js"
+  },
+  {
+    "revision": "18af2f56041340b0823e",
+    "url": "/static/js/runtime-main.459cbad6.js"
+  },
+  {
+    "revision": "7c59cca3ac2a131597b837848cf45869",
+    "url": "/static/media/bluebackground-04.7c59cca3.png"
+  },
+  {
+    "revision": "da6cce61228a205b22841de8504ef0ff",
+    "url": "/static/media/covid.da6cce61.png"
+  },
+  {
+    "revision": "61c8a755ba47d93ef91174abb37bd500",
+    "url": "/static/media/covid2.61c8a755.png"
+  },
+  {
+    "revision": "7f6216560d825dfd039bc51b626fd05a",
+    "url": "/static/media/covid3.7f621656.png"
+  },
+  {
+    "revision": "5145bbe7c87f3b86a430b2f45e6aa220",
+    "url": "/static/media/ex1.5145bbe7.png"
+  },
+  {
+    "revision": "e25f2da073a2b8f770e5ba833b084292",
+    "url": "/static/media/ex2.e25f2da0.png"
+  },
+  {
+    "revision": "8757a805074e79a01e48b2edb6f59f4e",
+    "url": "/static/media/fbg2.8757a805.png"
+  },
+  {
+    "revision": "e0ea730bb3e02f81bc8bcd2f0bc68029",
+    "url": "/static/media/guy1.e0ea730b.png"
+  },
+  {
+    "revision": "7a922c9ba0fc544a60d41010c5af112b",
+    "url": "/static/media/guy2.7a922c9b.png"
+  },
+  {
+    "revision": "fff0ba297a42e4973d46445995ca7e5b",
+    "url": "/static/media/logo.fff0ba29.png"
+  },
+  {
+    "revision": "7d3d2eb94e18aef375a8c9295c6133f9",
+    "url": "/static/media/logolight.7d3d2eb9.png"
+  },
+  {
+    "revision": "e17bb92254323d437aa5ab429df2287c",
+    "url": "/static/media/slide1.e17bb922.jpg"
+  },
+  {
+    "revision": "23a0a4d7343043385d8f8ff44e0efd05",
+    "url": "/static/media/wh1.23a0a4d7.png"
+  },
+  {
+    "revision": "e29503cfe7fe97dc751558f7bec9c915",
+    "url": "/static/media/wh2.e29503cf.png"
+  },
+  {
+    "revision": "0d8118989edb8a70ae7c8ab467a7a2f9",
+    "url": "/static/media/wh3.0d811898.png"
+  },
+  {
+    "revision": "503b87a68feee8849c8f76c5bd17958e",
+    "url": "/static/media/wh4.503b87a6.png"
+  },
+  {
+    "revision": "c56564eda7fdba5ee2c442225bf5dd6a",
+    "url": "/static/media/wh5.c56564ed.png"
+  }
+]);
